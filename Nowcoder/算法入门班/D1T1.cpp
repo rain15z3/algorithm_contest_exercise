cb5c1abfdd7f712c69ae78bf6d000f01fb5e551a
RainbowBird <rbxin2003@outlook.com>
2020-11-02 01:56:40 +0800

    Archive code

--- Nowcoder/算法入门班/D1T1.cpp
+++ Nowcoder/算法入门班/D1T1.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
+#include <algorithm>
 #include <cstring>
 using namespace std;
 
-int T, n, m;
 struct Tang {
     int a, b;
-} tang[50005];
-int c[50005];
+} tang[500005];
+
+int T, n, m;
+int c[500005];
 
 int main() {
     cin >> T;
@@ -19,58 +21,28 @@ int main() {
         for (int j = 1; j <= m; j++) {
             int a;
             cin >> a;
-            c[a] = 1;
+            c[a]++;
         }
-        
-        // 统计敌人数量
-        int enemy[50005], cura = 0, curb = 0;
-        for (int i = 1; i <= n; i++) {
+
+        // 求出第n秒所有人的战力
+        for (int i = n; i >= 1; i--) {
+            c[i] += c[i+1];
+            tang[i].b += c[i];
+        }
+
+        int cnt = 0, max1 = 0, max2 = 0;
+        for (int i = n; i >= 1; i--) {
             if (tang[i].a == 0) {
-                cura++;
-                enemy[i] = curb;
+                max1 = max(max1, tang[i].b);
+                if (max2 >= tang[i].b) cnt++;
             } else if (tang[i].a == 1) {
-                curb++;
-                enemy[i] = cura;
+                max2 = max(max2, tang[i].b);
+                if (max1 >= tang[i].b) cnt++;
             }
         }
-        
-        int diffa[50005], diffb[50005];
-        for (int t = 1; t <= n; t++) { // 游戏开始
-            // 发功
-            diffa[1] += c[t];
-            diffa[cura + 1] -= c[t];
-            diffb[1] += c[t];
-            diffb[curb + 1] -= c[t];
-            
-            // 攻击
-            for (int i = 1; i <= n; i++) {
-                if (tang[i].a == 0) {
-                    diffb[1] -= tang[i].b;
-                    diffb[enemy[i]+1] += tang[i].b;
-                } else if (tang[i].a == 1) {
-                    diffa[1] -= tang[i].b;
-                    diffa[enemy[i]+1] += tang[i].b;
-                }
-            }
-        }
-        
-        // 前缀和
-        int tot = n, adda = 0, addb = 0;
 
-        for (int i = 1; i <= cura; i++) {
-            adda += diffa[i];
-            tang[enemy[i]+1].b -= adda;
-            if (tang[enemy[i]+1].b <= 0) tot--;
-        }
-
-        for (int i = 1; i <= curb; i++) {
-            addb += diffb[i];
-            tang[enemy[i]+1].b -= addb;
-            if (tang[enemy[i]+1].b <= 0) tot--;
-        }
-        
-        cout << tot << endl;
+        cout << n - cnt << endl;
     }
-    
+
     return 0;
 }
